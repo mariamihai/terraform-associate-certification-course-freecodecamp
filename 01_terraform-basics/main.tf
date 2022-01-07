c46f706117d75c73fcff8c6ec241e2f48e687bc7
maria <maria.ic.mihai@gmail.com>
2022-01-07 15:50:33 +0100

    01 - TB - Added resource provider example

--- 01_terraform-basics/main.tf
+++ 01_terraform-basics/main.tf
@@ -12,6 +12,12 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
+provider "aws" {
+  profile = "terraform"
+  region  = "eu-west-2"
+  alias = "second_region"
+}
+
 variable "instance_type" {
   type = string
 }
@@ -27,6 +33,8 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "${local.project_name}_ExampleAppServerInstance"
   }
+
+  #provider = aws.second_region
 }
 
 module "vpc" {
@@ -35,6 +43,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
+  #azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
@@ -46,6 +55,10 @@ module "vpc" {
     Terraform   = "true"
     Environment = "dev"
   }
+
+  #providers = {
+  #  aws = aws.second_region
+  # }
 }
 
 output "public_ip" {

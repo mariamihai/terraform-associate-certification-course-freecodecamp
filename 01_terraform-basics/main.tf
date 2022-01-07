5e0cb9ab9b1611746b8d391fda6ca9b657c95bf4
maria <maria.ic.mihai@gmail.com>
2022-01-07 19:33:22 +0100

    01 - TB - Move to Terraform Cloud. Terraform and environment variables (instance_type, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY  and AWS_DEFAULT_REGION) need to be set.

--- 01_terraform-basics/main.tf
+++ 01_terraform-basics/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "mariamihai"
+
+    workspaces {
+      name = "terraform-basics"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"

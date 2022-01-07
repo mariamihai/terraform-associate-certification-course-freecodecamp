9afb283af6ceb0bd5e4073092261dcabe3989a4d
maria <maria.ic.mihai@gmail.com>
2022-01-07 13:08:17 +0100

    01 - TB - Added an EC2 instance with ami and instance type

--- 01_terraform-basics/main.tf
+++ 01_terraform-basics/main.tf
@@ -8,5 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  profile = "terraform"
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04dd4500af104442f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
